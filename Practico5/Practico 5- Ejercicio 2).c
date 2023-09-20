9a81c8f039cfce67d83fcbc30b429c3498422ba2
WensesV <102747677+WensesV@users.noreply.github.com>
2023-09-20 02:36:29 -0300

    Update Practico 5- Ejercicio 2).c
    Actualizada la tabla de ejecucion

--- Practico5/Practico 5- Ejercicio 2).c
+++ Practico5/Practico 5- Ejercicio 2).c
@@ -8,7 +8,7 @@ int main ( ) {
     Ci = varc-32;
     printf("El caracter es %c y su valor en la tabla ASCII es %d\n", varc, varc );
     printf("%c ----- %c", varc, varc-32 );
-    getchar();
+    getchar(); //No es necesario incluir esta linea. Esta agregada para que al compilarlo en Visual Studio Code nos muestre la pantalla final y se cierre con un enter.
     return (0);
 }
 
@@ -21,9 +21,17 @@ int main ( ) {
 Respuestas:
 2.1. Ci es una variable de tipo entero
      varv es una variable de tipo caracter
-2.2. No son los mismos objetos, uno es un numero entero y el otro es un caracter/letra. Tienen el mismo valor ya que q en la tabla ASCII tiene valor de 113, el cual es el mismo de la variable C
+2.2. No son los mismos objetos, uno es un numero entero y el otro es un caracter/letra. Tienen el mismo valor ya que 'q' en la tabla ASCII tiene valor de 113, el cual es el mismo valor numerico de la variable C. 
 2.3. Pantalla
     El caracter es q y su valor en la tabla ASCII es 113
     q ----- Q
-    2.4. Tabla de ejecucion
+
+2.4. Tabla de ejecucion
+|   Sentencias   |   C   |    Ci   |   varc   |                     Pantalla                       |
+|const char C='q'|(char)q|         |          |                                                    |
+|int Ci          |       |  (int)  |          |                                                    |
+|char varc=113   |       |         |   113    |                                                    |
+|Ci = varc-32    |       |    81   |          |                                                    |
+|prinf()         |       |         |          |El caracter es q y su valor en la tabla ASCII es 113|
+|prinf()         |       |         |          |q ----- Q                                           |
 */
