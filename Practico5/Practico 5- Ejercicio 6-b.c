d8b08236f5d6a622f6624bbf7e5ef969ca5f567f
WensesV <102747677+WensesV@users.noreply.github.com>
2023-09-20 04:35:46 -0300

    Update - Practico 5
    Agregados los ejercicios 6 y 9

---
+++ Practico5/Practico 5- Ejercicio 6-b.c
@@ -0,0 +1,73 @@
+/*
+6. Para cada uno de los items del ejercicio 3 se pide:
+
+a) Codificarlos en lenguaje C, mostrando por pantalla el contenido de todas las variables.
+b) Realizar el DF de cada uno de los programas escritos en lenguaje C.
+c) Pasarlo y ejecutarlo en CodeBlocks.
+d) Modificar el código a) para que el usuario ingrese los valores a las variables.
+e) Modificar el código b), implementar el mismo código con la sentencia for. */
+
+//Ejercicio 3-a)
+
+// a) Codificarlos en lenguaje C, mostrando por pantalla el contenido de todas las variables.
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int VE, ve;
+    VE= 0; ve = 0;
+    while (VE < 10) {
+        printf("Ciclo WHILE\n");
+        ve = ve+VE*10;
+        VE=VE+2;
+        printf("contenido de la variable VE: --- %d \n", VE);
+        printf("contenido de la variable ve: --- %d \n", ve);
+    }
+    printf("Fin del ciclo WHILE\n");
+    VE=0;
+    printf("contenido final de la variable VE: --- %d \n", VE);
+    printf("contenido final de la variable ve: --- %d \n", ve);
+    return 0;
+
+}
+
+//b), implementar el mismo código con la sentencia for. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int VE, ve;
+    ve = 0;
+    for (VE = 0; i < 10; VE=VE+2){
+        ve = ve+VE*10;
+    }
+    VE=0;
+    return 0;
+}
+*/
+
+//Version a)+e)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int VE, ve;
+    ve = 0;
+    for (VE = 0; VE < 10; VE=VE+2){
+        printf("Ciclo FOR\n");
+        ve = ve+VE*10;
+        printf("contenido de la variable VE: --- %d \n", VE);
+        printf("contenido de la variable ve: --- %d \n", ve);
+    }
+    printf("Fin del ciclo WHILE\n");
+    VE=0;
+    return 0;
+    printf("contenido final de la variable VE: --- %d \n", VE);
+    printf("contenido final de la variable ve: --- %d \n", ve);
+}
--- Practico5/Practico 5- Ejercicio 9.c
+++ Practico5/Practico 5- Ejercicio 9.c
@@ -1,3 +1,5 @@
+// 9. Dadas las siguientes sentencias y el valor asignado a cada una de las variables
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,5 +8,15 @@ int main()
     char x= 66; char y= 73; char j= x+3;
     printf ("Original: %c %c %c %c \n Modificada: %c %c %c %c \n ", x, y, j, j+9, x+32, y+32,
     j+32, j+9+32 );
+    getchar(); //No es necesario incluir esta linea. Esta agregada para que al compilarlo en Visual Studio Code nos muestre la pantalla final y se cierre con un enter.
     return 0;
 }
+
+// ¿Qué se muestra en la pantalla luego de ejecutar el printf?
+/*
+Original: B I E N
+ Modificada: b i e n
+*/
+
+
+
