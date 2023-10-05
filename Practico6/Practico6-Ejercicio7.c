eaabd6c3758b977fe37b825a3c73449e135d621e
WensesV <102747677+WensesV@users.noreply.github.com>
2023-10-05 15:51:01 -0300

    Update Practico6-Ejercicio7.c

--- Practico6/Practico6-Ejercicio7.c
+++ Practico6/Practico6-Ejercicio7.c
@@ -31,12 +31,13 @@ Nota 1, 2 y 3. (Si estuvo ausente se colocara un -1)
 cantidad de estudiantes es no mas de 120.
 
 ----- Version 1 -----
-T1 definir variables
+T1 Definir variables
 T2 Pedir la cantidad de alumnos ingresar (Maximo 120)
-Repetir las veces ingresadas
+Repetir las veces necesarias
     T3.1 Ingresar el DNI
     T3.2 Ingresar si es recursante o no
-    T3.3 Ingresar las notas de las 3 instancias
+    T3.3 Ingresar la cantidad de asistencias
+    T3.4 Ingresar las notas de las 3 instancias
 T4 Mostrar el porcentaje de estudiantes con menos de 5 asistencias.
 T5 Mostrar el promedio de alumnos recrusantes, sobre el total de estudiantes.
 T6 Mostrar la cantidad de aprobados en la instancia 1 con mas de 7 asistencias.
@@ -46,30 +47,27 @@ T8 Mostrar el numero de DNI y la cantidad de asistencia de todos los estudiantes
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{   int alumnos[120*5];
+int main(){
+    //T1 Definir variables
+    int alumnos[120*6];
     int i, k, cant;
     int j = 1;
 
     printf("Cuantos estudiantes desea ingresar? %d\n", j);
     scanf("%d",&cant);
     getchar();
-    while ((cant > 120) || (cant < 1))
-    {
+    while ((cant > 120) || (cant < 1)){
         printf("Error, ingrese un DNI valido para el alumno %d\n", j);
-        scanf("%d",&alumnos[i]);
+        scanf("%d",&cant);
         getchar();
-    }      
-
+    }
     //Ingreso de la informacion de cada estudiante
-    for (i = 0; i < 600; i = i+6)
-    {
+    for (i = 0; i < cant*6; i = i+6){
         //Ingreso DNI
         printf("Ingrese el DNI del estudiante %d\n", j);
         scanf("%d",&alumnos[i]);
         getchar();
-        while ((alumnos[i] > 99999999) || ((alumnos[i]) < 10000000))
-        {
+        while ((alumnos[i] > 99999999) || ((alumnos[i]) < 10000000)){
             printf("Error, ingrese un DNI valido para el alumno %d\n", j);
             scanf("%d",&alumnos[i]);
             getchar();
@@ -78,8 +76,7 @@ int main()
         printf("El alumno %d es recursante? S/N \n", j);
         scanf("%c",&alumnos[i+1]);
         getchar();
-        while ((alumnos[i+1] != 'S') && (alumnos[i+1] != 's') && (alumnos[i+1] != 'N') && (alumnos[i+1] != 'n') )
-        {
+        while ((alumnos[i+1] != 'S') && (alumnos[i+1] != 's') && (alumnos[i+1] != 'N') && (alumnos[i+1] != 'n') ){
             printf("Error, ingrese solo una 'S' o una 'N'\n", j);
             scanf("%c",&alumnos[i+1]);
             getchar();
