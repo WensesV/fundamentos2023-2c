db8504a4f57e0310391f61ad375fd49fb7c55dfc
WensesV <102747677+WensesV@users.noreply.github.com>
2023-10-05 15:21:19 -0300

    Update Practico6-Ejercicio7.c

--- Practico6/Practico6-Ejercicio7.c
+++ Practico6/Practico6-Ejercicio7.c
@@ -15,3 +15,120 @@ c) Cantidad de aprobados en la 1 instancia con más de 7 asistencias.
 d) Cantidad de Ausentes en la 1° instancia. 
 e) Mostrar por pantalla el número de DNI y la cantidad de asistencia de todos los
 estudiantes que aprobaron el 1er parcial.*/
+
+/*
+
+----Datos Auxiliares----
+
+DNI
+
+Cantidad de asistencias
+
+Si es o no Recursante
+
+Nota 1, 2 y 3. (Si estuvo ausente se colocara un -1)
+
+cantidad de estudiantes es no mas de 120.
+
+----- Version 1 -----
+T1 definir variables
+T2 Pedir la cantidad de alumnos ingresar (Maximo 120)
+Repetir las veces ingresadas
+    T3.1 Ingresar el DNI
+    T3.2 Ingresar si es recursante o no
+    T3.3 Ingresar las notas de las 3 instancias
+T4 Mostrar el porcentaje de estudiantes con menos de 5 asistencias.
+T5 Mostrar el promedio de alumnos recrusantes, sobre el total de estudiantes.
+T6 Mostrar la cantidad de aprobados en la instancia 1 con mas de 7 asistencias.
+T7 Mostrar la cantidad de ausentes en la primera instancia.
+T8 Mostrar el numero de DNI y la cantidad de asistencia de todos los estudiantes que aprobaron el primer parcial. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int alumnos[120*5];
+    int i, k, cant;
+    int j = 1;
+
+    printf("Cuantos estudiantes desea ingresar? %d\n", j);
+    scanf("%d",&cant);
+    getchar();
+    while ((cant > 120) || (cant < 1))
+    {
+        printf("Error, ingrese un DNI valido para el alumno %d\n", j);
+        scanf("%d",&alumnos[i]);
+        getchar();
+    }      
+
+    //Ingreso de la informacion de cada estudiante
+    for (i = 0; i < 600; i = i+6)
+    {
+        //Ingreso DNI
+        printf("Ingrese el DNI del estudiante %d\n", j);
+        scanf("%d",&alumnos[i]);
+        getchar();
+        while ((alumnos[i] > 99999999) || ((alumnos[i]) < 10000000))
+        {
+            printf("Error, ingrese un DNI valido para el alumno %d\n", j);
+            scanf("%d",&alumnos[i]);
+            getchar();
+        }
+        // Recursante si o no
+        printf("El alumno %d es recursante? S/N \n", j);
+        scanf("%c",&alumnos[i+1]);
+        getchar();
+        while ((alumnos[i+1] != 'S') && (alumnos[i+1] != 's') && (alumnos[i+1] != 'N') && (alumnos[i+1] != 'n') )
+        {
+            printf("Error, ingrese solo una 'S' o una 'N'\n", j);
+            scanf("%c",&alumnos[i+1]);
+            getchar();
+        }
+        // Cantidad de asistencias
+        printf("Ingrese la cantidad de asistencias del estudiante %d\n", j);
+        scanf("%d",&alumnos[i+2]);
+        getchar();
+        while ((alumnos[i+2] > 0) || ((alumnos[i+2]) < 15))
+        {
+            printf("Error, el maximo de asistencias es de 15\n", j);
+            scanf("%d",&alumnos[i+2]);
+            getchar();
+        }
+        //Ingreso de notas 1
+        for (k = 0; k < 3; k++)
+        {   //ingreso asistencia a la evaluacion
+            printf("El alumno %d estuvo presente en la instancia %d de evaluacion? S/N \n", k+1);
+            scanf("%d",&alumnos[i+3]);
+            getchar();
+            //Control de respuesta
+            while ((alumnos[i+3] != 'S') && (alumnos[i+3] != 's') && (alumnos[i+3] != 'N') && (alumnos[i+3] != 'n') )
+            {
+                printf("Error, ingrese solo una 'S' o una 'N'\n");
+                scanf("%c",&alumnos[i+1]);
+                getchar();
+            }
+            if ((alumnos[i+3] == 's') || (alumnos[i+3] == 'S'))
+            {   //Si estuvo presente ingresa la nota
+                printf("Ingrese el resultado de la evaluacion %d \n", k+1);
+                scanf("%d",&alumnos[i+(3+k)]);
+                getchar();
+                // Control de la nota
+                while ((alumnos[i+(3+k)] < 0) || (alumnos[i+(3+k)] > 10))
+                {
+                    printf("Nota mal ingresada, por favor, reintente\n");
+                    scanf("%d",&alumnos[i+(3+k)]);
+                    getchar();
+                }
+            }
+            else{ //Si estuvo ausente ingresa el -1
+                alumnos[i+(3+k)] = -1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
+
