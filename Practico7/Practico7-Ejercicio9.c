796157c1a68c46b0d7fde95fb91ad59856a172cb
WensesV <102747677+WensesV@users.noreply.github.com>
2023-10-29 20:12:54 -0300

    Create Practico7-Ejercicio9.c

---
+++ Practico7/Practico7-Ejercicio9.c
@@ -0,0 +1,69 @@
+Ejercicio 9)- 
+
+En un lavadero de autos se realizan no más de 250 lavados diarios. Se desea llevar el
+registro de los lavados diarios.
+Hay 4 lugares (Boxes) disponibles para lavar. Se lavan autos a $2000 c/u, camionetas a
+$2600 y Motos a $1000. Realizar un programa que permita ingresar por cada lavado, el
+lugar usado y el precio, en un único arreglo.
+Luego:
+
+a) Dado un lugar (Box) de lavado, ingresado por el usuario en la función main, calcular
+cuánto dinero se recaudó, durante todo el día, en dicho lugar y mostrar en la función main
+con el cartel adecuado.
+
+b) Calcular y mostrar para cada lugar (Box) el porcentaje de lavados realizados en ese
+lugar sobre el total de lavados realizados en el día.
+
+Definir las siguientes funciones:
+● Ingreso (ingresa la información necesaria).
+● RecaudaciónPorBox (calcula el total de dinero recaudado en un box).
+● PorcentajeDeLavados (calcula y muestra el porcentaje de la cantidad de lavados para
+UN Box sobre el total de lavados). Invocar tantas veces como sea necesario.
+
+Se pide:
+o Realizar la Versión 1 de la función main
+o Codificar en Lenguaje C (SIN UTILIZAR VARIABLES GLOBALES).
+o Pasar su código a CodeBlocks y ejecutar.
+
+----- Datos Auxiliares -----
+
+
+
+----- Version 1 -----
+
+T1 Definir variables
+T2 Preguntar cuantas cehiculos se van a almacenar
+Repetir las veces indicadas
+    T2 Preguntar el lugar (Box) donde se va a lavar el vehiculo
+    T2 Preguntar el tipo de vehiculo (Autos = $2000, Camionetas = $2600, Motos = $1000)
+    Almacenar lo pedido en una estructura de datos
+T3 Dado un lugar (Box), calcular lo recaudado en el dia y mostrar por pantalla
+T4 Calcular y mostrar el porcentaje de lavados realizados en cada lugar (Box)
+
+----- Lenguaje C -----
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int ingreso(){
+
+    return(0);
+}
+
+int recaudacionPorBox(){
+
+    return(0);
+}
+
+float porcentajeDeLavados(){
+    float porcentaje;
+    
+    return(porcentaje);
+}
+
+
+int main()
+{
+    
+    return 0;
+}
