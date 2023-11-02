5ce7bf6937d4f77939deca6fac19202d019dc44c
WensesV <102747677+WensesV@users.noreply.github.com>
2023-11-02 20:03:45 -0300

    Update C++
    Se agregaron dos ejercicios correspondientes al segundo parcial de la carrera de desarrollo web en la ULP. Proximamente saldran las guias de lenguaje C++ con estructuras, vectores, pilas y filas

---
+++ Lenguaje C++/Examenes ULP - Ejercicio 2.cpp
@@ -0,0 +1,133 @@
+/*Fila: paintBall
+
+Se tiene un Arma: pistola de balas de pintura, que se recarga por un tubo, donde se
+insertan las pelotitas de pintura verde fosforescente. En el orden que van
+ingresando las balas, se van a ir disparando(fifo).
+Cada bala de pintura tiene un código de dos dígitos que la identifica.
+El cargador del arma, se ve en las figuras.
+Se necesita
+
+A) un procedimiento “recarga10” balas y
+
+B) Otro “dispara1” del arma (valide que haya elementos).
+
+C) Entre las balas de pintura, una función “Buscar” si la próxima a disparar
+en el ARMA tiene el número ingresado por consola (cin)*/
+/*
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+    ArmaPaintBall {
+        vector<int> tubo;
+    }
+
+void recarga10() {
+    int codUlti = (tubo.empty()) ? 1 : tubo.back() + 1;
+
+    for(int i = 0; i < 10; i++){
+        tubo.push_back(codUlti++);
+    }
+    cout << "Se han recargado 10 balas de pintura en el tubo." << endl;
+}
+
+
+void dispara1() {
+    if(!tubo.empty()){
+        int bala = tubo.front();
+        tubo.erase(tubo.begin());
+        cout << "Se efectuo un disparo " << bala << endl;
+    }
+    else{
+        cout << "Tubo vacio, Imposible disparar" << endl;
+    }
+}
+
+bool buscar(int numero) {
+    if (!tubo.empty()) {
+        int proximaBala = tubo.front();
+        if (proximaBala == numero) {
+            cout << "Se encontro la bala, procederemos a dispararla" << numero << endl;
+            dispara1();
+            return true;
+        }
+    }
+    else{
+        cout << "El tubo está vacío. Debes recargar." << endl;
+    }
+    return false;
+}
+
+
+
+int main(){
+    ArmaPaintBall pistola;
+
+    pistola.recarga10();
+    pistola.dispara1();
+    cout << "Ingrese el codigo de la bala a buscar" << endl;
+    cin >> codigo;
+    pistola.buscar(codigo);
+return 0;
+}
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ArmaPaintBall {
+private:
+    vector<int> tubo;
+
+public:
+    void recarga10() {
+        int codUlti = (tubo.empty()) ? 1 : tubo.back() + 1;
+
+        for (int i = 0; i < 10; i++) {
+            tubo.push_back(codUlti++);
+        }
+        cout << "Se han recargado 10 balas de pintura en el tubo." << endl;
+    }
+
+    void dispara1() {
+        if (!tubo.empty()) {
+            int bala = tubo.front();
+            tubo.erase(tubo.begin());
+            cout << "Se disparo " << bala << endl;
+        } else {
+            cout << "Tubo vacio, imposible disparar." << endl;
+        }
+    }
+
+    bool buscar(int num) {
+        if (!tubo.empty()) {
+            int proxBala = tubo.front();
+            if (proxBala == num) {
+                cout << "Se encontró la bala, procederemos a dispararla." << endl;
+                dispara1();
+                return true;
+            }
+        } else {
+            cout << "El tubo está vacío. Debes recargar." << endl;
+        }
+        return false;
+    }
+};
+
+int main() {
+    ArmaPaintBall pistola;
+    int codigo;
+
+    pistola.recarga10();
+    pistola.dispara1();
+
+    cout << "Ingrese el código de la bala a buscar: ";
+    cin >> codigo;
+
+    pistola.buscar(codigo);
+
+    return 0;
+}
